b09ba2009b2e78209f8bb625c52533c19243736e
DESKTOP-65SAUST\Alumno <andreasalasr@gmail.com>
2025-01-23 15:22:59 +0100

    CoinFlipping

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
 
-        console.log("Online JQuery");
-        
+    let $coin = $("#coin");
 
+    $coin.on("click", function(){
+        let randomNum = Math.random();     
+        $coin.removeClass(); //borra la clase que tenga para poder asignarle una nueva y q no pete la animacion
+
+        setTimeout(function(){
+
+            if(randomNum <= 0.5){
+                $coin.addClass("isHeads");
+            }else{
+                $coin.addClass("isTails");
+            }
+
+        }, 100);
+
+    });
 
 });
